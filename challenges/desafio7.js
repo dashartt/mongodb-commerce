510a9b603ba47b6e3f0ddd3a2c27b9ab3813efe5
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-08 15:09:05 -0300

    add: requirement 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,14 @@
+db.produtos
+    .find(
+        {
+            $and: [
+              { vendidos: { $ne: 50 } },
+              { tags: { $exists: false } },
+            ],
+        }, 
+        { 
+            _id: false,
+            nome: true,
+            vendidos: true,            
+        },
+    );
