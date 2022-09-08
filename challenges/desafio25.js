3fbaa8d039b8c2d01f1fc9f40005e57a3cd8e020
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-08 18:32:01 -0300

    add: requirement 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,30 @@
+db.produtos.updateMany(
+    {
+        valoresNutricionais: {
+            $elemMatch: {
+              $and: [
+                { percentual: { $gte: 40 }},
+                { tipo: 'sódio' },
+              ],
+            },
+        },
+    },
+    {
+        $push: {
+            tags: {  
+                $each: ['muito sódio'],
+            },
+        },
+    },
+);
+
+db.produtos.find(
+    {
+
+    },
+    {
+        _id: false,
+        nome: true,        
+        tags: true,
+    },
+);
