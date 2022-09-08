806029a7567357eb77d94373a41663f4d9f748b3
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-08 16:04:50 -0300

    add: requirement 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+    {
+        ingredientes: {
+            $all: ['picles']
+        }
+    },
+    {
+      _id: false,
+      nome: true,
+      ingredientes: true,      
+      valoresNutricionais: { $slice: 3 }
+    },
+  );
