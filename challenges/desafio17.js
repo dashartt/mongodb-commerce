78d1569795e4e999075b14be545006125bc74b69
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-08 19:13:48 -0300

    add: requirement 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,7 @@
+db.resumoProdutos.insertOne(
+    {
+        franquia: 'McDonalds',
+        totalProdutos: db.produtos.find().count(),
+    },
+);
+db.resumoProdutos.find({}, { _id: false, franquia: true, totalProdutos: true });
