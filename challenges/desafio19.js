f060cf0cfc87c8e6566b60b342507168b51e660b
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-09-08 16:53:32 -0300

    add: requirement 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+    {
+        
+    },
+    {
+        $pull: {
+            ingredientes: {
+                $in: ['cebola']
+            },
+        },
+    },
+);
+
+db.produtos.find({ }, { _id: false, nome: true, ingredientes: true })
